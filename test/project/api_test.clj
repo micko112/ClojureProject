732f8b2ebe28c1951af35a4c416e41aaffdf129e
micko112 <dimitrijemitic112@gmail.com>
2026-01-23 00:35:56 +0100

    Change to clojure.test, done 2 tests

--- test/project/api_test.clj
+++ test/project/api_test.clj
@@ -1,16 +1,19 @@
 (ns project.api-test
-  (:require [midje.sweet :refer :all]
+  (:require
             [project.db :as db]
             [project.leaderboard :as lb]
             [datomic.api :as d]
             [database.schema :as schema]
             [database.seed :as seed]
             [project.api :as api]
+            [clojure.test :refer :all]
+
             )
   (:import (java.time.temporal TemporalAdjusters)
            (java.util Date)
            (java.time ZonedDateTime LocalDate ZoneId)
-           (java.time Instant LocalDate DayOfWeek MonthDay YearMonth ZoneId)))
+           (java.time Instant LocalDate DayOfWeek MonthDay YearMonth ZoneId))
+)
 
 (def old-lb
   [{:user/username "A" :rank 1}
@@ -29,28 +32,49 @@
     {:user/username "C" :user/xp 150}
 
    ])
-()
-(fact "test delta funkcije"
-  (let [delta (lb/leaderboard-delta-edge-case old-lb new-lb)]
-    delta)
-  => [{:user/username "A" :delta -1 }
-      {:user/username "B" :delta -2 }
-      {:user/username "C" :delta 0 }
-      {:user/username "D" :delta 3 }]
-)
+(def fake-db
+  {:users
+   users})
 
-(fact "New user check"
-      (lb/new-user-check old-lb new-lb)
-      => )
 
-(defn new-user-check [old-lb new-lb]
-  (let [old-names (set(lb/get-all-names old-lb)) new-names (set(lb/get-all-names new-lb))]
-    (filter (complement old-names) new-names))
+(deftest leaderboard-rank-ties-test
+  (with-redefs
+    [project.db/get-all-users
+     (fn [_]
+       [{:user/username "A"}
+        {:user/username "B"}
+        {:user/username "C"}])
 
-)
+     project.db/xp-per-user
+     (fn [_ username _ _]
+       ({"A" 300 "B" 200 "C" 150} username))]
 
-(fact "test leaderboard unit fn"
-      (let []))
+    (is (= (lb/leaderboard-rank-ties ::db :daily ::date)
+           [{:user/username "A" :user/xp 300 :rank 1}
+            {:user/username "B" :user/xp 200 :rank 2}
+            {:user/username "C" :user/xp 150 :rank 3}]))))
+(deftest leaderboard-rank-ties-test
+  (with-redefs
+    [project.db/get-all-users
+     (fn [_]
+       [{:user/username "A"}
+        {:user/username "B"}
+        {:user/username "C"}])
 
+     project.db/xp-per-user
+     (fn [_ username _ _]
+       ({"A" 300 "B" 300 "C" 150} username))]
+
+    (is (= (lb/leaderboard-rank-ties ::db :daily ::date)
+           [{:user/username "A" :user/xp 300 :rank 1}
+            {:user/username "B" :user/xp 300 :rank 1}
+            {:user/username "C" :user/xp 150 :rank 3}]))))
+
+(deftest delta-test
+  (is (= (lb/leaderboard-delta-edge-case old-lb new-lb)
+         [{:user/username "A" :delta -1}
+          {:user/username "B" :delta -2}
+          {:user/username "C" :delta 0}
+          {:user/username "D" :delta 3}])))
 
 
